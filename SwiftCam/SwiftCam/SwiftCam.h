349016d2ba20d1d27cc60aba31dab00341402e9c
David-Okun <david.okun@ibm.com>
2017-04-21 16:11:39 -0400

    Initial commit with workspace

---
+++ SwiftCam/SwiftCam/SwiftCam.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCam.h
+//  SwiftCam
+//
+//  Created by David Okun IBM on 4/21/17.
+//  Copyright © 2017 David Okun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCam.
+FOUNDATION_EXPORT double SwiftCamVersionNumber;
+
+//! Project version string for SwiftCam.
+FOUNDATION_EXPORT const unsigned char SwiftCamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCam/PublicHeader.h>
+
+
