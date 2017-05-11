c71083ac8556e74d937cd76f32b22b3cbfdecf6e
David-Okun <david.okun@ibm.com>
2017-05-11 11:55:14 -0500

    Fixed issue with orientation of returned images

--- Lumina/Lumina/LuminaController.swift
+++ Lumina/Lumina/LuminaController.swift
@@ -298,7 +298,7 @@ private extension CMSampleBuffer {
             print("Could not create CoreGraphics image from context")
             return nil
         }
-        return UIImage(cgImage: graphicsImage)
+        return UIImage(cgImage: graphicsImage, scale: 1.0, orientation: .right).fixOrientation
     }
     
     var imageFromPixelBuffer: UIImage? {
@@ -333,7 +333,8 @@ private extension CMSampleBuffer {
                     return nil
                 }
                 CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
-                return UIImage(cgImage: sample)
+                
+                return UIImage(cgImage: sample, scale: 1.0, orientation: .right).fixOrientation
             } else {
                 print("Could not create CoreGraphics context")
                 return nil
@@ -462,5 +463,66 @@ extension LuminaController: AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         delegate.detected(camera: self, data: metadataObjects)
+    }
+}
+
+private extension UIImage {
+    var fixOrientation: UIImage {
+        if imageOrientation == UIImageOrientation.up {
+            return self
+        }
+        
+        var transform: CGAffineTransform = CGAffineTransform.identity
+        
+        switch imageOrientation {
+        case UIImageOrientation.down, UIImageOrientation.downMirrored:
+            transform = transform.translatedBy(x: size.width, y: size.height)
+            transform = transform.rotated(by: CGFloat.pi)
+            break
+        case UIImageOrientation.left, UIImageOrientation.leftMirrored:
+            transform = transform.translatedBy(x: size.width, y: 0)
+            transform = transform.rotated(by: CGFloat.pi / 2)
+            break
+        case UIImageOrientation.right, UIImageOrientation.rightMirrored:
+            transform = transform.translatedBy(x: 0, y: size.height)
+            transform = transform.rotated(by: CGFloat.pi / -2)
+            break
+        case UIImageOrientation.up, UIImageOrientation.upMirrored:
+            break
+        }
+        
+        switch imageOrientation {
+        case UIImageOrientation.upMirrored, UIImageOrientation.downMirrored:
+            transform.translatedBy(x: size.width, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+            break
+        case UIImageOrientation.leftMirrored, UIImageOrientation.rightMirrored:
+            transform.translatedBy(x: size.height, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+        case UIImageOrientation.up, UIImageOrientation.down, UIImageOrientation.left, UIImageOrientation.right:
+            break
+        }
+        
+        let ctx: CGContext = CGContext(data: nil,
+                                       width: Int(size.width),
+                                       height: Int(size.height),
+                                       bitsPerComponent: self.cgImage!.bitsPerComponent,
+                                       bytesPerRow: 0,
+                                       space: self.cgImage!.colorSpace!,
+                                       bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!
+        
+        ctx.concatenate(transform)
+        
+        switch imageOrientation {
+        case UIImageOrientation.left, UIImageOrientation.leftMirrored, UIImageOrientation.right, UIImageOrientation.rightMirrored:
+            ctx.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.height, height: size.width))
+        default:
+            ctx.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            break
+        }
+        
+        let cgImage: CGImage = ctx.makeImage()!
+        
+        return UIImage(cgImage: cgImage)
     }
 }
