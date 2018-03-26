64d8093f6050e6df641b0c8501adb66cbd6ae5b1
Gerriet Backer <gerriet@einbacker.com>
2018-03-26 15:16:44 +0200

    Fixed whitespace

--- Lumina/Lumina/UI/LuminaViewController.swift
+++ Lumina/Lumina/UI/LuminaViewController.swift
@@ -207,7 +207,7 @@ public final class LuminaViewController: UIViewController {
     public func setCancelButton(visible: Bool) {
         cancelButton.isHidden = !visible
     }
-    
+
     public func setShutterButton(visible: Bool) {
         shutterButton.isHidden = !visible
     }
@@ -215,11 +215,11 @@ public final class LuminaViewController: UIViewController {
     public func setSwitchButton(visible: Bool) {
         switchButton.isHidden = !visible
     }
-    
+
     public func setTorchButton(visible: Bool) {
         torchButton.isHidden = !visible
     }
-    
+
     /// A collection of model types that will be used when streaming images for object recognition
     ///
     /// - Note: Only works on iOS 11 and up
